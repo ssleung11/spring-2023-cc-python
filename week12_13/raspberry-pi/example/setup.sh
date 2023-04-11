cc1b68fa0b51b4e48f9a1c4fbcd975cd97f6a602
ssleung11 <42116150+ssleung11@users.noreply.github.com>
2023-04-10 22:26:33 -0400

    week 12 and 13

---
+++ week12_13/raspberry-pi/example/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip3 install -r requirements.txt 
+sudo apt-get -y install libportaudio0 
+sudo apt-get -y install libportaudio2 
+sudo apt-get -y install libportaudiocpp0 
+sudo apt-get -y install portaudio19-dev
+sudo apt-get -y install mpg123
